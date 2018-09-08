fb929336166e910040996b4b82504d48299c8375
Nicole Hilden <nicole.hilden@gmail.com>
2018-09-07 21:37:15 -0400

    Triangle code is working now

--- Triangle.py
+++ Triangle.py
@@ -29,8 +29,8 @@ def classifyTriangle(a,b,c):
 
     # verify that all 3 inputs are integers  
     # Python's "isinstance(object,type) returns True if the object is of the specified type
-    if not((isinstance(a,int)) and (isinstance(b,int)) and (isinstance(c,int))):
-        return 'InvalidInput'
+    if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
+        return 'InvalidInput';
     
     # require that the input values be >= 0 and <= 200
     if a > 200 or b > 200 or c > 200:
@@ -38,7 +38,7 @@ def classifyTriangle(a,b,c):
         
     if a <= 0 or b <= 0 or c <= 0:
         return 'InvalidInput'
-    
+      
     # This information was not in the requirements spec but 
     # is important for correctness
     # the sum of any two sides must be strictly less than the third side
@@ -49,7 +49,7 @@ def classifyTriangle(a,b,c):
     # now we know that we have a valid triangle 
     if a == b and b == c:
         return 'Equilateral'
-    elif ((((a ** 2) + (b ** 2)) == (c ** 2)) or (((a ** 2) + (c ** 2)) == (b ** 2)) or (((c ** 2) + (b ** 2)) == (a ** 2))):
+    elif (((a ** 2) + (b ** 2)) == (c ** 2)) or (((a ** 2) + (c ** 2)) == (b ** 2)) or (((c ** 2) + (b ** 2)) == (a ** 2)):
         return 'Right'
     elif (a != b) and  (b != c) and (a != c):
         return 'Scalene'
